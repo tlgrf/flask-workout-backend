9062a3f39e5a61423529e732e4beaf891e6308c4
Ridge <ridge.telegrafi@icloud.com>
2025-07-25 04:44:17 -0400

    Add Marshmallow schemas with validations

---
+++ server/schemas.py
@@ -0,0 +1,30 @@
+from marshmallow import Schema, fields, validate, validates_schema, ValidationError
+
+class ExerciseSchema(Schema):
+    id               = fields.Int(dump_only=True)
+    name             = fields.Str(required=True, validate=validate.Length(min=3))
+    category         = fields.Str(required=True,
+                           validate=validate.OneOf(['strength','cardio','flexibility','balance']))
+    equipment_needed = fields.Bool(required=True)
+
+class WorkoutSchema(Schema):
+    id               = fields.Int(dump_only=True)
+    date             = fields.Date()
+    duration_minutes = fields.Int(required=True, validate=validate.Range(min=1))
+    notes            = fields.Str()
+
+class WESchema(Schema):
+    reps             = fields.Int(validate=validate.Range(min=1))
+    sets             = fields.Int(validate=validate.Range(min=1))
+    duration_seconds = fields.Int(validate=validate.Range(min=1))
+
+    @validates_schema
+    def check_one(self, data, **kwargs):
+        if not ((data.get('reps') and data.get('sets')) or data.get('duration_seconds')):
+            raise ValidationError('Must include reps+sets or duration_seconds')
+
+exercise_schema  = ExerciseSchema()
+exercises_schema = ExerciseSchema(many=True)
+workout_schema   = WorkoutSchema()
+workouts_schema  = WorkoutSchema(many=True)
+we_schema        = WESchema()
